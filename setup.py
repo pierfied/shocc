82247a8deee2da7c5ab99b7f468028648bfd89de
= <pierfied@email.arizona.edu>
2020-10-06 12:26:58 -0700

    Fixed setup.py to compile using the cmake file.

--- setup.py
+++ setup.py
@@ -1,7 +1,68 @@
-from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+import subprocess
+import shutil
+import sys
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-HEALPIX_DIR = '/home/pierfied/Downloads/Healpix_3.70_2020Jul23/Healpix_3.70'
+# CMake tools slightly modified from https://github.com/raydouglass/cmake_setuptools.
+
+CMAKE_EXE = os.environ.get('CMAKE_EXE', shutil.which('cmake'))
+
+
+def check_for_cmake():
+    if not CMAKE_EXE:
+        print('cmake executable not found. '
+              'Set CMAKE_EXE environment or update your path')
+        sys.exit(1)
+
+
+class CMakeExtension(Extension):
+    """
+    setuptools.Extension for cmake
+    """
+
+    def __init__(self, name, sourcedir=''):
+        check_for_cmake()
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+        self.target = name.split('.')[-1]
+
+
+class CMakeBuildExt(build_ext):
+    """
+    setuptools build_exit which builds using cmake & make
+    You can add cmake args with the CMAKE_COMMON_VARIABLES environment variable
+    """
+
+    def build_extension(self, ext):
+        check_for_cmake()
+        if isinstance(ext, CMakeExtension):
+            output_dir = os.path.abspath(
+                os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+            build_type = 'Debug' if self.debug else 'Release'
+            cmake_args = [CMAKE_EXE,
+                          ext.sourcedir,
+                          '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + output_dir,
+                          '-DCMAKE_BUILD_TYPE=' + build_type]
+            cmake_args.extend(
+                [x for x in
+                 os.environ.get('CMAKE_COMMON_VARIABLES', '').split(' ')
+                 if x])
+
+            env = os.environ.copy()
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+            subprocess.check_call(cmake_args,
+                                  cwd=self.build_temp,
+                                  env=env)
+            subprocess.check_call(['make', '-j', ext.target],
+                                  cwd=self.build_temp,
+                                  env=env)
+            print()
+        else:
+            super().build_extension(ext)
 
 setup(
     name='SHoCC',
@@ -12,17 +73,7 @@ setup(
     author='Pier Fiedorowicz',
     author_email='pierfied@email.arizona.edu',
     description='Spherical Harmonics on CUDA Cards',
-    ext_modules=[
-        CUDAExtension(
-            name='shocc.driver',
-            sources=['src/shocc.cu', 'src/standard_transforms.cu'],
-            include_dirs=[HEALPIX_DIR + '/include'],
-            extra_objects=[HEALPIX_DIR + '/lib/libhealpix_cxx.a'],
-            extra_compile_args={'cxx': [],
-                                'nvcc': ['-O3']}
-        )
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    ext_modules=[CMakeExtension('shocc.driver')],
+    cmdclass={'build_ext': CMakeBuildExt},
+    zip_safe=False
 )
